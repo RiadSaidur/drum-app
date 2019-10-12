8960ba69527b2b32b41f1a18166645619d98c6bb
deathcrush <reidvai1998@gmail.com>
2019-10-12 13:15:38 +0600

    fixed: multiple record

--- beats-script.js
+++ beats-script.js
@@ -1,31 +1,16 @@
 window.addEventListener('load', ()=>{
   const kits = document.querySelectorAll('.sound');
   const beats = document.querySelectorAll('.beats div');
-  // console.log(beats);
+
   let groove = [];
   let stop=false;
   let i=0;
-
-  function beatBox(){
-      for(let i=0; i< groove.length; i++){
-          kits[i].play();
-      }
-  }
-
-  function preRecord(){
-    beats.forEach((beat, index)=>{
-      beat.addEventListener('click', function(){
-        if(stop) return;
-        groove.push(index);
-        console.log(index);
-      });
-    });  
-  }
 
   beats.forEach((beat, index)=>{
       beat.addEventListener('click', function(){
           kits[index].currentTime = 0;
           kits[index].play();
+          groove.push(index);
       });
   });
 
@@ -33,26 +18,27 @@ window.addEventListener('load', ()=>{
     if(i==groove.length) i=0;
     if(i<groove.length && !stop) setTimeout(recorded, 1000);
     kits[groove[i]].play();
-    console.log(groove[i]);
+    kits[groove[i]].currentTime = 0;
     i++;
     stop=false;
-    console.log(stop);
+    console.log(i);
   }
 
   document.querySelector('.play').addEventListener('click', ()=>{
     stop=false;
-    console.log(stop);
     recorded();
   });
-  // document.querySelector('.clear').addEventListener('click', ()=>{
-  //   groove=[];
-  // });
+
   document.querySelector('.record').addEventListener('click', ()=>{
+    i=0;
     stop=false;
-    preRecord();
+    groove=[];
+
   });
+
   document.querySelector('.stop').addEventListener('click', ()=>{
     stop=true;
   });
+
 });
 
